891e383c50c15cca6b02dc226c5defe5ddb6ab59
Lucas Rocha <lucasgabriels2210@gmail.com>
2023-07-07 15:47:53 -0300

    posts limit

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 
 // o máximo de posts pesquisados por página
-const LIMIT = 1;
+const LIMIT = 5;
 
 export async function getServerSideProps(context) {
   const postsQuery = query(
